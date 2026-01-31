a411f222395125944643357f439c038f12e3e0dd
javelinsman <javelinsman@snu.ac.kr>
2026-01-30 22:22:27 -0500

    Build

--- interactive-slides/vite.config.js
+++ interactive-slides/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/HAI2026-Interactive-Slides/',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+  }
 })
